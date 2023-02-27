16e015cbd5ee2a74136883921e257d7f4b49d837
keonwoo98 <keonwoo98@naver.com>
2023-02-27 21:10:03 +0900

    Feat: CORS error 해결중..

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -3,6 +3,7 @@ import loadable from '@loadable/component';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 const LogIn = loadable(() => import('../../pages/LogIn/LogIn'));
+const LoginCheck = loadable(() => import('../../pages/LoginCheck/LoginCheck'));
 const SignUp = loadable(() => import('../../pages/SignUp/SignUp'));
 const Home = loadable(() => import('../Home/Home'));
 const Chat = loadable(() => import('../../pages/Chat/Chat'));
@@ -12,12 +13,20 @@ const TwoFactor = loadable(() => import('../../pages/TwoFactor/TwoFactor'));
 // const Home = loadable(() => import('@layouts/Home'));
 
 const App = () => {
+  // const accessToken = getCookies('access_token');
+  // const twoFactorChecked = getCookies('two_factor_auth');
+
+  // if (!accessTocken)
+  //  return ();
+  // if (!twoFactorChecked)
+  //   return ();
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Navigate replace to="/login" />} />
           <Route path="/login" element={<LogIn />} />
+          <Route path="/logincheck" element={<LoginCheck />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/twofactor" element={<TwoFactor />} />
           <Route path="/home" element={<Home />} />
--- src/layouts/Home/Home.tsx
+++ src/layouts/Home/Home.tsx
@@ -2,7 +2,18 @@ import React, { useCallback, useEffect, useState } from 'react';
 import loadable from '@loadable/component';
 import { useNavigate, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
-import { Container, MainContainer, ProfileContainer, Button, Nav, Label, Input, Workspaces, WorkspaceButton, AddButton, Div } from './styles';
+import {
+  Container,
+  MainContainer,
+  ProfileContainer,
+  Button,
+  Nav,
+  Label,
+  Input,
+  Workspaces,
+  WorkspaceButton,
+  Div,
+} from './styles';
 import GlobalStyles from '../../styles/global';
 import home from '../../assets/home.svg';
 import game from '../../assets/game.svg';
@@ -14,33 +25,95 @@ import setting from '../../assets/setting.svg';
 const Channel = loadable(() => import('../../pages/Channel/Channel'));
 
 const Home = () => {
+  // const clientId = import.meta.env.VITE_CLIENT_ID;
+  // const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+  // const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+
+  // useEffect(() => {
+  //   const url = new URL(window.location.href);
+  //   const authorizationCode = url.searchParams.get('code');
+
+  //   if (authorizationCode) {
+  //     const data = {
+  //       grant_type: 'authorization_code',
+  //       client_id: clientId,
+  //       client_secret: clientSecret,
+  //       code: authorizationCode,
+  //       redirect_uri: redirectUri,
+  //     };
+
+  //     fetch('https://api.intra.42.fr/oauth/token', {
+  //       method: 'POST',
+  //       body: JSON.stringify(data),
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     })
+  //       .then((response) => {
+  //         console.log(response);
+  //         if (!response.ok) {
+  //           throw new Error('Failed to retrieve access token');
+  //         }
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         const refreshToken = data.refresh_token;
+  //         const accessToken = data.access_token;
+  //         console.log(data);
+  //         const headers = {
+  //           Authorization: `Bearer ${accessToken}`,
+  //         };
+  //         fetch('https://api.intra.42.fr/v2/me', {
+  //           headers,
+  //         })
+  //           .then((response) => {
+  //             if (!response.ok) {
+  //               throw new Error('Failed to retrieve user data');
+  //             }
+  //             return response.json();
+  //           })
+  //           .then((data) => console.log(data))
+  //           .catch((error) => console.error(error));
+  //       })
+  //       .catch((error) => console.error(error))
+  //       .finally(() => {
+  //         // Remove the 'code' parameter from the URL
+  //         window.history.replaceState(
+  //           {},
+  //           document.title,
+  //           window.location.pathname
+  //         );
+  //       });
+  //   }
+  // }, [clientId, clientSecret, redirectUri]);
+
   const navigate = useNavigate();
-  const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] = useState(false);
+  const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] =
+    useState(false);
 
   const onClickLogOut = () => {
     navigate('/');
-  }
+  };
 
   const onClickGame = () => {
     navigate('/game');
-  }
+  };
 
   const onClickChat = () => {
     navigate('/chat');
-  }
+  };
 
   const onClickCreateWorkspace = useCallback(() => {
     setShowCreateWorkspaceModal(true);
   }, []);
 
   const data = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
       .then((response) => response.json())
       .then((data) => console.log(data));
-  }
+  };
 
-
-  return  (
+  return (
     <div>
       <GlobalStyles />
       <Container bg='#00E5FF'>
@@ -48,12 +121,24 @@ const Home = () => {
         {/* <h1>Main Page!</h1> */}
         <Div>
           <Workspaces>
-            <WorkspaceButton><img src={home}></img></WorkspaceButton>
-            <WorkspaceButton onClick={onClickGame}><img src={game}></img></WorkspaceButton>
-            <WorkspaceButton onClick={onClickChat}><img src={chat}></img></WorkspaceButton>
-            <WorkspaceButton><img src={setting}></img></WorkspaceButton>
-            <WorkspaceButton onClick={onClickLogOut}><img src={logout}></img></WorkspaceButton>
-            <WorkspaceButton onClick={onClickCreateWorkspace}>+</WorkspaceButton>
+            <WorkspaceButton>
+              <img src={home}></img>
+            </WorkspaceButton>
+            <WorkspaceButton onClick={onClickGame}>
+              <img src={game}></img>
+            </WorkspaceButton>
+            <WorkspaceButton onClick={onClickChat}>
+              <img src={chat}></img>
+            </WorkspaceButton>
+            <WorkspaceButton>
+              <img src={setting}></img>
+            </WorkspaceButton>
+            <WorkspaceButton onClick={onClickLogOut}>
+              <img src={logout}></img>
+            </WorkspaceButton>
+            <WorkspaceButton onClick={onClickCreateWorkspace}>
+              +
+            </WorkspaceButton>
           </Workspaces>
           <MainContainer bg='#00E5FF'>
             <Nav>○ ○ ○</Nav>
@@ -71,6 +156,5 @@ const Home = () => {
       </Container>
     </div>
   );
-}
-
+};
 export default Home;
