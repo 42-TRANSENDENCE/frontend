22f5d167867b4568689704969d19c49bce5bb293
Hyoshik Lee <hyoslee@c3r6s3.42seoul.kr>
2023-02-27 16:44:39 +0900

    hyoslee_local 통합(주의: 앞으로 패키지  설치시 --legacy-peer-deps 옵션을 추가해야 합니다)

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -1,30 +1,40 @@
-import React from 'react';
-import loadable from '@loadable/component';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React from "react";
+import loadable from "@loadable/component";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const LogIn = loadable(() => import('../../pages/LogIn/LogIn'));
-const SignUp = loadable(() => import('../../pages/SignUp/SignUp'));
-const Home = loadable(() => import('../Home/Home'));
-const Chat = loadable(() => import('../../pages/Chat/Chat'));
-const Game = loadable(() => import('../../pages/Game/Game'));
-const TwoFactor = loadable(() => import('../../pages/TwoFactor/TwoFactor'));
+const queryClient = new QueryClient();
+
+const LogIn = loadable(() => import("../../pages/LogIn/LogIn"));
+const SignUp = loadable(() => import("../../pages/SignUp/SignUp"));
+const Home = loadable(() => import("../Home/Home"));
+const Chat = loadable(() => import("../../pages/Chat/Chat"));
+const Game = loadable(() => import("../../pages/Game/Game"));
+const TwoFactor = loadable(() => import("../../pages/TwoFactor/TwoFactor"));
 // const LogIn = loadable(() => import('@ pages/LogIn'));
 // const Home = loadable(() => import('@layouts/Home'));
 
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/login" />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/twofactor" element={<TwoFactor />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/game" element={<Game />} />
-        </Routes>
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/login" />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/twofactor" element={<TwoFactor />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/chat/*" element={<Chat />} />
+            <Route path="/game" element={<Game />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 };
