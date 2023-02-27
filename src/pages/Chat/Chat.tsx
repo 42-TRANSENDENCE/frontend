aa2bb932b17560fbd573d746239e3fc9794b2762
Hyoshik Lee <hyoslee@c3r6s3.42seoul.kr>
2023-02-27 17:05:10 +0900

    Revert "Revert "hyoslee_local 통합(주의: 앞으로 패키지  설치시 --legacy-peer-deps 옵션을 추가해야 합니다)""
    This reverts commit 3c85c345aa06130a5a3a1e1c8911de7225cef8e3.

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Container, Button, Nav, Label, Input } from '../../styles/styles';
-import GlobalStyles from '../../styles/global';
+import React from "react";
+import { Container, Button, Nav, Label, Input } from "../../styles/styles";
+import GlobalStyles from "../../styles/global";
+import { Navigate, Route, Routes } from "react-router";
+import V2rooms from "./v2_rooms";
+import V2dms from "./v2_dms";
+import V2chats from "./v2_chats";
+import CreateRoom from "./v2_create_room";
+
+// export const chat_backurl = "http://127.0.0.1:3095";
 
 const Chat = () => {
   return (
-    <div>
-      <GlobalStyles />
-      <Container bg='#00E5FF'>
-        <Nav>○ ○ ○</Nav>
-        <h1>Chat Page!</h1>
-        <h1>...</h1>
-      </Container>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate replace to="v2_rooms" />} />
+      <Route path="v2_rooms/create_room" element={<CreateRoom />} />
+      <Route path="v2_rooms/:roomId/chat" element={<V2chats />} />
+      <Route path="v2_rooms/*" element={<V2rooms />} />
+      <Route path="v2_dms/:dmId" element={<V2dms />} />
+    </Routes>
   );
-}
+};
 
 export default Chat;
