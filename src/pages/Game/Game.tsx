94fad4895eacbb6f51ea799af415cf17807fbffe
ulol <jeffk0408@naver.com>
2023-03-05 08:25:49 +0900

    feat : Game 페이지

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,18 +1,55 @@
-import React from 'react';
-import { Container, Button, Nav, Label, Input } from '../../styles/styles';
-import GlobalStyles from '../../styles/global';
+import {useState, useContext, useEffect} from 'react'
+import { GameContext } from '../../contexts/GameSocket';
+import { GameState } from '../../components/Game/enums';
+import { GameLobby } from '../../components/Game/GameLobby';
+import { GameWaiting } from '../../components/Game/GameWaiting';
+import { GamePlay } from '../../components/Game/GamePlay';
 
-const Game = () => {
-  return (
-    <div>
-      <GlobalStyles />
-      <Container bg='#00E5FF'>
-        <Nav>○ ○ ○</Nav>
-        <h1>Game Page!</h1>
-        <h1>...</h1>
-      </Container>
-    </div>
-  );
+const Game = () : JSX.Element => {
+  const [gamestate, setGamestate] = useState(GameState.Lobby);
+  const [room, setRoom] = useState(null);
+  const socket = useContext(GameContext);
+
+  useEffect(() => {
+    console.log(" [ RENDERING ] : game page : ")
+    socket.emit("lobby_initialize");
+    return () => {
+      socket.emit("lobby_terminate")
+      socket.emit("quit_queue");
+      console.log(" [ STOP ] : game page")
+    }
+  }, [])
+
+  useEffect(() => {
+    socket.on("joined_to_queue", () => {
+      console.log("qeueue");
+      setGamestate(GameState.Waiting);
+    });
+    socket.on("out_of_queue", () => {
+      setGamestate(GameState.Lobby);
+    })
+    socket.on("enter_to_game", (roomId) => {
+      setRoom(roomId)
+      console.log("room :",room, roomId);
+      setGamestate(GameState.InGame);
+    })
+    return () => {
+      socket.off("joined_to_queue");
+      socket.off("out_of_queue");
+      socket.off("enter_to_game");
+    }
+  }, [])
+
+  switch (gamestate) {
+    case GameState.Lobby :
+      return (<GameLobby socket={socket}/>);
+    case GameState.Waiting :
+      return (<GameWaiting socket={socket}/>);
+    case GameState.InGame :
+      return (<GamePlay socket={socket} roomId={room} setGamestate={setGamestate}/>);
+    default :
+      return (<>ERROR</>)
+  }
 }
 
-export default Game;
+export default Game
