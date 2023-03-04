1f917bb95207fa3ea704985b04d7e2e51ee893b7
ulol <jeffk0408@naver.com>
2023-03-05 08:25:27 +0900

    feat : Game에 필요한 컴포넌트

---
+++ src/components/Game/JoinButton.tsx
@@ -0,0 +1,25 @@
+import '../../styles/Game/JoinButton.css'
+import { Socket } from 'socket.io-client'
+
+const JoinButton = (props : any) => {
+  const socket : Socket = props.socket;
+  const iswaiting : boolean = props.iswaiting;
+
+  function join_clicked () {
+    if (iswaiting) {
+      socket.emit("quit_queue");
+      console.log("취소 버튼 누름");
+    } else {
+      socket.emit("join_queue");
+      console.log("게임 시작 버튼 누름");
+    }
+  }
+  
+  return (
+      <div className='joinbutton' onClick={join_clicked} >
+        { (iswaiting) ? <p>Cancel</p> : <p>Join Game</p> }
+      </div>
+  )
+}
+
+export default JoinButton
