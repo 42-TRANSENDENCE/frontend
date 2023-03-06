09657b418b07969e8eecb89c039c5417c3de29b0
dkim2 <60467872+u-lo-l@users.noreply.github.com>
2023-03-06 21:50:26 +0900

    feat : change socket addr to AWS public

--- src/contexts/GameSocket.tsx
+++ src/contexts/GameSocket.tsx
@@ -1,7 +1,7 @@
 import { createContext } from "react"
 import {io, Socket} from 'socket.io-client'
 
-export const gameSocket = io("ws://localhost:3001/game", {
+export const gameSocket = io("ws://44.195.129.81:8181/game", {
                               transports: ["websocket"]
                             });
 
