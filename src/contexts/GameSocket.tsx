aa93ff5964d4f8827f83365c9332c49f5050442e
ulol <jeffk0408@naver.com>
2023-03-08 01:43:22 +0900

    feat : 자잘한 오류 수정

--- src/contexts/GameSocket.tsx
+++ src/contexts/GameSocket.tsx
@@ -1,15 +1,9 @@
 import { createContext } from "react"
 import {io, Socket} from 'socket.io-client'
-const Protocol = "ws"
-// const ServerPublicIp = process.env.MY_PUBLIC_IP;
 const ServerPublicIp = "localhost";
-const ServerPort = "8181";
-const GameNamespace = "game"
+const ServerPort = "3000";
 
-const SocketAddr = Protocol+"://"+ServerPublicIp+":"+ServerPort+"/"+GameNamespace
-// export const gameSocket = io("ws://44.195.129.81:8181/game", {
-//                               transports: ["websocket"]
-//                             });
+const SocketAddr = "ws://"+ServerPublicIp+":"+ServerPort+"/game"
 
 export const gameSocket = io(SocketAddr, {
                               transports: ["websocket"]
