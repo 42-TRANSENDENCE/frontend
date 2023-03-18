46c38a6592e29ed30a107aa4a85ff443045ec2f0
EC2 Default User <ec2-user@ip-172-31-15-40.ec2.internal>
2023-03-18 07:43:35 +0000

    Delete: old files

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,10 +1,12 @@
 import {useState, useContext, useEffect} from 'react'
 import { GameContext } from '../../contexts/GameSocket';
-import { GameState } from './components/enums';
-import { GameLobby } from './components/GameLobby';
-import { GameWaiting } from './components/GameWaiting';
-import { GamePlay } from './components/GamePlay';
-import './styles/Game.css'
+import Title from '../../components/Title';
+import { GameState } from './enum';
+
+import Lobby from './components/Lobby';
+import Waiting from './components/Waiting';
+import Playing from './components/Playing';
+import { GameContainer } from './styles'
 
 const Game = () : JSX.Element => {
   const [gamestate, setGamestate] = useState(GameState.Lobby);
@@ -41,20 +43,30 @@ const Game = () : JSX.Element => {
     }
   }, [])
 
+  const GameByState = (): JSX.Element => {
+    if (gamestate === GameState.Lobby)
+      return <Lobby socket={socket} />;
+    else if (gamestate === GameState.Waiting)
+      return <Waiting socket={socket} />;
+    else if (gamestate === GameState.InGame)
+      return (
+        <Playing socket={socket} roomId={room} setGamestate={setGamestate} />
+      );
+    return <h1>ERROR</h1>;
+  };
+
   return (
-    <div className='game__container'>
-      {
-        (gamestate === GameState.Lobby) ? (
-          <GameLobby socket={socket}/>
-        ) : ( (gamestate === GameState.Waiting) ? (
-          <GameWaiting socket={socket}/>
-        ) : ( (gamestate === GameState.InGame) ? (
-          <GamePlay socket={socket} roomId={room} setGamestate={setGamestate}/>
-        ) : (
-          null
-        )))
-      }
-    </div>
+    <GameContainer>
+      <div className="Title">
+          <Title title="PONG GAME" home={true}/>
+      </div>
+      <div className="Body">
+        <GameByState "Body"/>
+      </div>
+      <div className="Title">
+          <Title title=""/>
+      </div>
+    </GameContainer>
   )
 }
 
---
+++ src/pages/Game/components/Playing.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Playing = () : JSX.Element => {
+    return (
+        <div>
+        </div>
+    )
+};
+
+export default Playing;
