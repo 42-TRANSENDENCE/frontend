78dafe6af52bd3f1bce156ac44d8372466604cd4
teetee6 <teetee6@naver.com>
2023-03-21 05:58:19 +0900

    미디어쿼리 미적용. 엉성한 채팅 디자인

--- src/components/ChatList/index.tsx
+++ src/components/ChatList/index.tsx
@@ -1,62 +1,180 @@
 import {
   ChatBubble,
-  ChatBubbleTail,
+  // ChatBubbleTail,
   ChatItem,
   ChatLists,
+  ChatMain,
   ChatProfile,
-} from "./styles";
+} from './styles';
+import chatSendButtonUrl from '../../assets/smallButton/chatSendButton.svg';
+import { useState } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+import React from 'react';
 
-const ChatList = ({ Flex }) => {
+const ChatsData = [
+  {
+    imgSrc: 'https://via.placeholder.com/50',
+    name: 'me',
+    content:
+      '  Hi there!Hi there!Hi there!Hi there!Hi there!Hi there!Hithere!Hi there! Hi there!Hi there!Hi there!Hi there!Hi there!Hithere! Hi there!Hi there!Hi there!Hi there!Hi there!Hi there!Hithere!Hi there!Hi there!Hi there!Hi there!Hi there!Hi there! Hithere!Hi there! Hi there! Hi there!',
+  },
+  {
+    imgSrc: 'https://via.placeholder.com/50',
+    name: 'other1',
+    content:
+      'zip your mouth up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+  },
+  {
+    imgSrc: 'https://via.placeholder.com/50',
+    name: 'me',
+    content: 'Yes sir!',
+  },
+];
+
+const ChatListComponent = React.memo(({ chatList }: { chatList: any }) => {
+  return (
+    <ChatLists>
+      {chatList.map((chatData: any) => (
+        <ChatItem other={chatData.name !== 'me'}>
+          <ChatProfile>
+            <img src={chatData.imgSrc} alt="User profile" />
+          </ChatProfile>
+          <ChatMain>
+            <span>{chatData.name}</span>
+            <ChatBubble
+              other={chatData.name !== 'me'}
+              style={{ whiteSpace: 'pre-wrap' }}
+            >
+              {chatData.content}
+            </ChatBubble>
+          </ChatMain>
+        </ChatItem>
+      ))}
+    </ChatLists>
+  );
+});
+
+const ChatList = ({ Flex }: { Flex: number }) => {
+  const [chat, setChat] = useState('');
+  const [chatList, setChatList] = useState(ChatsData);
+
+  const onChangeChat = (e: any) => {
+    setChat(e.target.value);
+  };
+
+  const onSubumitChat = (e: any) => {
+    e.preventDefault();
+    if (!chat?.trim()) return;
+    // console.log(chat);
+    /** nickname을 이용한 Chat 위치 */
+    setChatList([
+      ...chatList,
+      { imgSrc: 'https://via.placeholder.com/50', name: 'me', content: chat },
+    ]);
+    setChat('');
+  };
+
+  function handleKeyDown(event: any) {
+    if (event.keyCode === 13 && !event.shiftKey) {
+      // Submit the form
+      event.preventDefault();
+      setChatList([
+        ...chatList,
+        { imgSrc: 'https://via.placeholder.com/50', name: 'me', content: chat },
+      ]);
+      setChat('');
+    } else if (event.keyCode === 13 && event.shiftKey) {
+      // Add a new line
+      event.preventDefault();
+      setChat((prevChat) => prevChat + '\n');
+    }
+  }
+
   return (
     <div
       style={{
         flex: Flex,
-        borderRadius: "2rem",
-        border: "0.3rem solid black",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
+        borderRadius: '2rem',
+        border: '0.3rem solid black',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
       <div style={{ flex: 9 }}>
-        <ChatLists>
-          <ChatItem>
-            <ChatProfile>
-              <span>John Doe</span>
-              <img src="https://via.placeholder.com/50" alt="User profile" />
-            </ChatProfile>
-            <ChatBubble>
-              Hi there!Hi there!Hi there!Hi there!Hi there!Hi there!Hi there!Hi
-              there! Hi there!Hi there!Hi there!Hi there!Hi there!Hi there! Hi
-              there!Hi there!Hi there!Hi there!Hi there!Hi there!Hi there!Hi
-              there!Hi there!Hi there!Hi there!Hi there!Hi there! Hi there!Hi
-              there! Hi there! Hi there!
-              <ChatBubbleTail />
-            </ChatBubble>
-          </ChatItem>
-          <ChatItem other>
-            <ChatBubble other>
-              Hey!
-              <ChatBubbleTail other />
-            </ChatBubble>
-            <ChatProfile>
-              <span>Jane Doe</span>
-              <img src="https://via.placeholder.com/50" alt="User profile" />
-            </ChatProfile>
-          </ChatItem>
-          <ChatItem>
-            <ChatProfile>
-              <span>John Doe</span>
-              <img src="https://via.placeholder.com/50" alt="User profile" />
-            </ChatProfile>
-            <ChatBubble>
-              Hi there!
-              <ChatBubbleTail />
-            </ChatBubble>
-          </ChatItem>
-        </ChatLists>
+        <Scrollbars>
+          {/* <ChatLists>
+            {chatList.map((chatData) => (
+              <ChatItem other={chatData.name !== 'me'}>
+                <ChatProfile>
+                  <img src={chatData.imgSrc} alt="User profile" />
+                </ChatProfile>
+                <ChatMain>
+                  <span>{chatData.name}</span>
+                  <ChatBubble
+                    other={chatData.name !== 'me'}
+                    style={{ whiteSpace: 'pre-wrap' }}
+                  >
+                    {chatData.content}
+                  </ChatBubble>
+                </ChatMain>
+              </ChatItem>
+            ))}
+          </ChatLists> */}
+          <ChatListComponent chatList={chatList} />
+        </Scrollbars>
       </div>
-      <div style={{ flex: 1 }}>b-2</div>
+      <form
+        style={{
+          flex: 1,
+          width: '100%',
+          display: 'flex',
+          marginBottom: '1rem',
+        }}
+        onSubmit={onSubumitChat}
+      >
+        <textarea
+          style={{
+            flex: 8,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '2rem',
+            border: '0.3rem solid black',
+            padding: '1rem',
+            overflow: 'hidden',
+            resize: 'none',
+          }}
+          onChange={onChangeChat}
+          onKeyDown={handleKeyDown}
+          value={chat}
+        ></textarea>
+        <div
+          style={{
+            flex: 1,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <button
+            style={{
+              padding: '0.5rem',
+              borderRadius: '1rem',
+              backgroundColor: '#FCF451',
+              border: '0.3rem solid black',
+              width: '4rem',
+              height: '4rem',
+            }}
+            onClick={onSubumitChat}
+          >
+            <img
+              src={chatSendButtonUrl}
+              style={{ maxWidth: '100%', maxHeight: '100%' }}
+            />
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/ChatList/styles.tsx
+++ src/components/ChatList/styles.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ChatLists = styled.div`
   display: flex;
@@ -8,43 +8,41 @@ export const ChatLists = styled.div`
 export const ChatItem = styled.div<{ other?: boolean }>`
   margin: 0.5rem;
   display: flex;
-  justify-content: ${(props) => (props.other ? "flex-end" : "flex-start")};
+  justify-content: 'flex-start';
+  flex-direction: ${(props) => (props.other ? 'row-reverse' : 'row')};
   position: relative;
 `;
 
 export const ChatBubble = styled.div<{ other?: boolean }>`
   position: relative;
   padding: 1.5rem;
-  max-width: 80%;
+  max-width: 97%;
   border-radius: 1rem;
-  background-color: ${(props) => (props.other ? "#FFF" : "#F0F0F0")};
-  color: ${(props) => (props.other ? "#000" : "#555")};
-`;
+  background-color: ${(props) => (props.other ? '#FFF' : '#F0F0F0')};
+  color: ${(props) => (props.other ? '#000' : '#555')};
 
-export const ChatBubbleTail = styled.div<{ other?: boolean }>`
-  position: absolute;
-  width: 0;
-  height: 0;
-  border-style: solid;
-  border-width: 0.625rem;
-  border-color: ${(props) =>
-    props.other
-      ? "#FFF transparent transparent transparent"
-      : "#F0F0F0 transparent transparent transparent"};
-  bottom: -0.625rem;
-  ${(props) => (props.other ? "right" : "left")}: -0.625rem;
-  transform: ${(props) => (props.other ? "rotate(-45deg)" : "rotate(45deg)")};
+  &:after {
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 0.625rem;
+    border-color: ${(props) =>
+      props.other
+        ? '#FFF transparent transparent transparent'
+        : '#F0F0F0 transparent transparent transparent'};
+    top: -0.63rem;
+    ${(props) => (props.other ? 'right' : 'left')}: -0.625rem;
+    transform: ${(props) =>
+      props.other ? 'rotate(-135deg)' : 'rotate(135deg)'};
+  }
 `;
 
 export const ChatProfile = styled.div<{ other?: boolean }>`
   display: flex;
-  flex-direction: column;
-  align-items: center;
   margin-bottom: 0.5rem;
-  /* position: relative; */
-  position: absolute;
-  bottom: 0;
-  ${(props) => (props.other ? "right" : "left")}: 0;
+  /* ${(props) => (props.other ? 'right' : 'left')}: 0; */
 
   & > img {
     width: 2rem;
@@ -52,8 +50,9 @@ export const ChatProfile = styled.div<{ other?: boolean }>`
     border-radius: 50%;
     margin-right: 0.5rem;
   }
+`;
 
-  & > span {
-    font-weight: bold;
-  }
+export const ChatMain = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
