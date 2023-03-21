d22d2b286ae3608e85c1c262b89067303022dac1
keonwoo98 <keonwoo98@naver.com>
2023-03-21 11:59:47 +0900

    Feat: 2FA useRef

--- src/pages/TwoFactor/TwoFactor.tsx
+++ src/pages/TwoFactor/TwoFactor.tsx
@@ -9,12 +9,6 @@ import loginButton from '../../assets/bigButton/2FALoginButton.svg';
 
 const TwoFactor = () => {
   const awsUrl = import.meta.env.VITE_AWS_URL;
-  const p1 = useInput('');
-  const p2 = useInput('');
-  const p3 = useInput('');
-  const p4 = useInput('');
-  const p5 = useInput('');
-  const p6 = useInput('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const [twoFactorError, setTwoFactorError] = useState(false);
@@ -120,3 +114,88 @@ const TwoFactor = () => {
 };
 
 export default TwoFactor;
+
+
+
+// import React, { useState, useCallback, useRef, useEffect } from 'react';
+// import { Container, Label, Input, Inputs, Form, Conflict } from './styles';
+// import Button from '../../components/Button';
+// import Title from '../../components/Title';
+// import loginButton from '../../assets/bigButton/2FALoginButton.svg';
+// import useInput from '../../hooks/useInput';
+
+// const TwoFactor = () => {
+//   const [password, setPassword] = useState('');
+//   const ref1 = useRef<HTMLInputElement>(null);
+//   const ref2 = useRef<HTMLInputElement>(null);
+//   const ref3 = useRef<HTMLInputElement>(null);
+//   const ref4 = useRef<HTMLInputElement>(null);
+//   const ref5 = useRef<HTMLInputElement>(null);
+//   const ref6 = useRef<HTMLInputElement>(null);
+
+//   const handleOnChange = useCallback((index: number, value: string) => {
+//     if (value.length === 1) {
+//       setPassword((prevPassword) => {
+//         const newPassword = prevPassword.slice(0, index) + value + prevPassword.slice(index + 1);
+//         return newPassword;
+//       });
+//       switch (index) {
+//         case 0:
+//           ref2.current?.focus();
+//           break;
+//         case 1:
+//           ref3.current?.focus();
+//           break;
+//         case 2:
+//           ref4.current?.focus();
+//           break;
+//         case 3:
+//           ref5.current?.focus();
+//           break;
+//         case 4:
+//           ref6.current?.focus();
+//           break;
+//         case 5:
+//           ref6.current?.blur();
+//           break;
+//         default:
+//           break;
+//       }
+//     }
+//     else {
+//       setPassword((prevPassword) => {
+//         const newPassword = prevPassword.slice(0, index) + prevPassword.slice(index + 1);
+//         return newPassword;
+//       });
+//     }
+//   }, []);
+
+//   const inputProps = useInput('', (value: string) => /^[0-9]{0,1}$/.test(value));
+
+//   useEffect(() => {
+//     ref1.current?.focus();
+//   }, []);
+
+//     const onSubmit = useCallback(
+//       (e: any) => {
+//         e.preventDefault();
+//         console.log(password);
+//       },
+//       [password]
+//   );
+//   return (
+//     <Form onSubmit={onSubmit}>
+//       <Inputs>
+//         <Input type='text' maxLength={1} ref={ref1} {...inputProps} onChange={(event) => handleOnChange(0, event.target.value)} />
+//         <Input type='text' maxLength={1} ref={ref2} {...inputProps} onChange={(event) => handleOnChange(1, event.target.value)} />
+//         <Input type='text' maxLength={1} ref={ref3} {...inputProps} onChange={(event) => handleOnChange(2, event.target.value)} />
+//         <Input type='text' maxLength={1} ref={ref4} {...inputProps} onChange={(event) => handleOnChange(3, event.target.value)} />
+//         <Input type='text' maxLength={1} ref={ref5} {...inputProps} onChange={(event) => handleOnChange(4, event.target.value)} />
+//         <Input type='text' maxLength={1} ref={ref6} {...inputProps} onChange={(event) => handleOnChange(5, event.target.value)} />
+//       </Inputs>
+//       <Button img_url={loginButton} type='submit' />
+//     </Form>
+//   );
+// };
+
+// export default TwoFactor;
