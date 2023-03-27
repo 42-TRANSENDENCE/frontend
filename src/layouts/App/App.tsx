559f1c510b13028e3e623dc29dc93e3d5380f831
EC2 Default User <ec2-user@ip-172-31-15-40.ec2.internal>
2023-03-27 11:01:49 +0000

    Feat: Private router 구성

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -7,15 +7,18 @@ import {
 } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { gameSocket, GameContext } from '../../contexts/GameSocket';
 import { AppContainer } from './styles';
 
-const LogIn = loadable(() => import("../../pages/LogIn/LogIn"));
-const SignUp = loadable(() => import("../../pages/SignUp/SignUp"));
-const Home = loadable(() => import("../Home/Home"));
-const Chat = loadable(() => import("../../pages/Chat/Chat"));
-const Game = loadable(() => import("../../pages/Game/Game"));
-const TwoFactor = loadable(() => import("../../pages/TwoFactor/TwoFactor"));
+import PrivateRoute from './PrivateRoute';
+const LogIn = loadable(() => import('../../pages/LogIn/LogIn'));
+const SignUp = loadable(() => import('../../pages/SignUp/SignUp'));
+const Home = loadable(() => import('../Home/Home'));
+const Chat = loadable(() => import('../../pages/Chat/Chat'));
+const Game = loadable(() => import('../../pages/Game/Game'));
+const TwoFactor = loadable(() => import('../../pages/TwoFactor/TwoFactor'));
 
 const queryClient = new QueryClient();
 const App = () => {
@@ -23,17 +26,29 @@ const App = () => {
     <AppContainer>
       <QueryClientProvider client={queryClient}>
         {/* <GameContext.Provider value={gameSocket}> */}
-          <Router>
-            <Routes>
-              <Route path="/" element={<Navigate replace to="/login" />} />
-              <Route path="/login" element={<LogIn />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/twofactor" element={<TwoFactor />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/chat/*" element={<Chat />} />
-              <Route path="/game" element={<Game />} />
-            </Routes>
-          </Router>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/login" />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/twofactor" element={<PrivateRoute component={TwoFactor} />} />
+            <Route path="/home" element={<PrivateRoute component={Home} />} />
+            <Route path="/chat/*" element={<PrivateRoute component={Chat} />} />
+            <Route path="/game" element={<PrivateRoute component={Game} />} />
+          </Routes>
+          <ToastContainer
+            position='top-right'
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme='dark'
+          />
+        </Router>
         {/* </GameContext.Provider> */}
       </QueryClientProvider>
     </AppContainer>
