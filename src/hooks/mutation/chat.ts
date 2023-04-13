ae52382de51b113021d2a6ee41ca54e06c96609c
EC2 Default User <ec2-user@ip-172-31-11-70.ap-northeast-2.compute.internal>
2023-04-13 19:04:32 +0900

    chore: socket emit 삭제

--- src/hooks/mutation/chat.ts
+++ src/hooks/mutation/chat.ts
@@ -1,5 +1,5 @@
-import { useFetcher } from "../fetcher";
-import { useMutation, UseMutationResult, MutationFunction, useQueryClient } from "react-query";
+import { useFetcher } from '../fetcher';
+import { useMutation, UseMutationResult, MutationFunction, useQueryClient } from 'react-query';
 import { Socket } from 'socket.io-client';
 import { toast } from 'react-toastify';
 
@@ -13,6 +13,11 @@ export interface CreateChannelData {
   title: string;
   password: string;
   socket: Socket | undefined;
+}
+
+interface PostChatData {
+  id: string;
+  chat: string;
 }
 
 export function useCreatChannel(): UseMutationResult<void, Error, CreateChannelData, MutationFunction<void, CreateChannelData>> {
@@ -35,12 +40,12 @@ export function useCreatChannel(): UseMutationResult<void, Error, CreateChannelD
       .then(response => {
         if (response.status === 201) {
           toast.success('New channel created');
-          socket?.on('joinChannel', (data: any) => {
-            socket?.emit('joinChannel', {'channelId': data.id});
-          });
-          return () => {
-            socket?.off('joinChannel');
-          }
+          // socket?.on('newChannel', (data: any) => {
+          //   socket?.emit('joinChannel', {'channelId': data.id});
+          // });
+          // return () => {
+          //   socket?.off('newChannel');
+          // }
         }
         else if (response.status === 400)
           toast.error('Channel name alreay exists')
@@ -62,12 +67,12 @@ export function useJoinChannel(): UseMutationResult<void, Error, JoinChannelData
 
 	async function joinChannel(data: JoinChannelData): Promise<void> {
 		const { id, password, socket } = data;
-		await fetcher("/channels/" + id, {
-			method: "POST",
+		await fetcher('/channels/' + id, {
+			method: 'POST',
 			headers: {
-				"Content-Type": "application/json",
+				'Content-Type': 'application/json',
 			},
-			credentials: "include",
+			credentials: 'include',
 			body: JSON.stringify({ password: password }),
 		})
       .then((response) => {
@@ -86,6 +91,38 @@ export function useJoinChannel(): UseMutationResult<void, Error, JoinChannelData
     mutationFn: joinChannel,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['myChannels'] });
+    }
+  });
+}
+
+export function usePostChat(): UseMutationResult<void, Error, PostChatData, MutationFunction<void, PostChatData>> {
+	const queryClient = useQueryClient();
+	const fetcher = useFetcher();
+
+	async function postChat(data: PostChatData): Promise<void> {
+		const { id, chat } = data;
+		await fetcher('/chat/' + id, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			credentials: 'include',
+			body: JSON.stringify({ chat: chat }),
+		})
+      .then((response) => {
+			if (response.status === 201)
+        toast.success('Successfully posted chat ' + id);
+      // else if (response.status === 400)
+      //   toast.error('You are already in the channel');
+      // else if (response.status === 403)
+      //   toast.error('Wrong password')
+		});
+	}
+
+	return useMutation({
+    mutationFn: postChat,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [''] });
     }
   });
 }
