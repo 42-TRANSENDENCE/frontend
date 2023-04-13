d8732fd4720e6eac0e016a7615cf34c1a2b0da6a
EC2 Default User <ec2-user@ip-172-31-11-70.ap-northeast-2.compute.internal>
2023-04-13 14:52:05 +0900

    feat: 채널 클릭 시 채팅창 Pop

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -15,11 +15,13 @@ import ChatRoom from '../../components/ChatRoom';
 import OnlineList from '../../components/OnlineList';
 import Title from '../../components/Title';
 import { Channels, MyChannels } from '../../components/Channels';
+import Chatting from '../../components/Chatting';
 import { Container } from '../../layouts/Home/styles';
 import { Link } from 'react-router-dom';
 
 const Chat = () => {
   const [chat_socket, disconnect_chat_socket] = useSocket('channelchat');
+  const [popChatting, setPopChatting] = useState(false);
   console.log('connecting chat_socket: ', chat_socket);
 
   useEffect(() => {
@@ -43,12 +45,15 @@ const Chat = () => {
             </div>
             
             <div className='MiddleSide Section'>
-              <MyChannels socket={chat_socket} />
-              {/* <Chatting /> */}
+              {popChatting ? (
+                <Chatting socket={chat_socket} setPopChatting={setPopChatting} />
+                ) : (
+                <MyChannels socket={chat_socket} setPopChatting={setPopChatting} />
+              )}
             </div>
 
             <div className='RightSide Section'>
-              <Channels socket={chat_socket} />
+              <Channels socket={chat_socket} setPopChatting={setPopChatting} />
               {/* <ChatRoom socket={chat_socket} /> */}
             </div>
           </div>
