d23aaa71a34e51ac92f254763d97b322c68c2d9d
ulol_wsl <jeffk0408@naver.com>
2023-04-15 20:04:08 +0900

    feat : 초대 수신, 수락기능 추가

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,12 +1,34 @@
-import { useCallback } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { Scrollbars } from "react-custom-scrollbars";
 import { useReceivedFriendList, usePendingFriendList } from '../../hooks/query/friend';
 import { useApproveFriend, useDeleteRequestFriend, useRefuseFriend } from '../../hooks/mutation/friend';
 import { UserInfo } from '../../hooks/query/user';
 import { NotificationContainer } from './styles';
 import Button from '@mui/material/Button';
+import { SocketContext } from '../../contexts/ClientSocket';
+import {GameMode} from '../../pages/Game/enum';
+import { ClientStatus } from '../OnlineList';
+import { useNavigate } from 'react-router-dom';
+
+export interface ClientInfo
+{
+  socketId : string;
+  user : UserInfo;
+  roomId: string | undefined;
+  status : ClientStatus;
+}
+
+interface InvitationInfo
+{
+  from: ClientInfo;
+  to: ClientInfo;
+  mode: GameMode;
+  roomId: string;
+}
 
 function Notification() {
+  const navigate = useNavigate();
+  const clientSocket = useContext(SocketContext);
   const userFriendReceived = useReceivedFriendList().data;
   const friendReceivedList: UserInfo[] = userFriendReceived;
   const userFriendPending = usePendingFriendList().data;
@@ -14,6 +36,7 @@ function Notification() {
   const approveFriend = useApproveFriend();
   const refuseFriend = useRefuseFriend();
   const deleteRequestFriend = useDeleteRequestFriend();
+  const [invitationList, setInvitationList] = useState<InvitationInfo[] | null>(null);
 
   const onClickApproveFriend = useCallback(
     (id: number) => {
@@ -32,6 +55,36 @@ function Notification() {
       deleteRequestFriend.mutate(id);
     }, [friendPendingList, deleteRequestFriend]
   );
+
+  const onClickAcceptInvitation = (inviteInfo : InvitationInfo) => {
+    console.log(inviteInfo)
+    clientSocket.emit("accept", inviteInfo);
+  };
+
+  const onClickRefuseInvitation = useCallback(
+    () => {
+
+    }, []
+  );
+
+  useEffect(() => {
+    clientSocket.on('updateInviteList', (data : InvitationInfo[] | null) => {
+      setInvitationList(data);
+    });
+
+    clientSocket.on('match_maked', (data : any) => {
+      console.log('match_maked :', data.roomId);
+      navigate('/game/play', {state: {room: data.roomId}});
+    });
+
+    clientSocket.emit("getinvitaionlist");
+    
+    return () => {
+      clientSocket.off('invited');
+      clientSocket.off('match_maked');
+    }
+  }, [])
+
   return (
     <NotificationContainer>
       <Scrollbars autoHide style={{}} onScrollFrame={() => { }}>
@@ -65,6 +118,23 @@ function Notification() {
             </>
           )
         })}
+        {invitationList?.map((inviteInfo : InvitationInfo, index : number) => {
+          return (
+            <div key={index}>
+              <h3>Invitation From "{inviteInfo.from.user.nickname}"</h3>
+              <div className='Notification'>
+                <span>{inviteInfo.mode} Mode</span>
+                <Button variant="contained" color="success" onClick={() => onClickAcceptInvitation(inviteInfo)}>
+                  Approve
+                </Button>
+                <Button variant="contained" color="error" onClick={() => onClickRefuseInvitation()}>
+                  Refuse
+                </Button>
+              </div>
+            </div>
+          )
+        })
+        }
       </Scrollbars>
     </NotificationContainer >
   )
