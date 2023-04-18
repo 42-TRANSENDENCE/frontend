896d49a222d551630f5ac727e742b312398181f4
ulol_wsl <jeffk0408@naver.com>
2023-04-18 20:27:54 +0900

    feat : 게임 소개 모달 추가

--- src/pages/Game/Lobby/index.tsx
+++ src/pages/Game/Lobby/index.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
-import { BigButton } from "../../../components/Button";
+import { useEffect, useState } from "react";
+import { BigButton, SmallButton } from "../../../components/Button";
 import normalButton from "../../../assets/bigButton/normalButton.svg";
 import chaosButton from "../../../assets/bigButton/chaosButton.svg";
+import modalQuestionButton from "../../../assets/smallButton/modalQuestionButton.svg";
+import Modal from "../../../components/Modal";
 import { GameMode } from "../enum";
 
 const Lobby = (props: any): JSX.Element => {
   const client_socket = props.socket;
+  const [gameManual, setGameManual] = useState<boolean>(false);
 
   useEffect(() => {
     console.log("GameLobby 입장");
@@ -16,7 +19,6 @@ const Lobby = (props: any): JSX.Element => {
 
   function normal_game_clicked() {
     console.log("normal game button clicked");
-    // data : QueueDto
     client_socket.emit("join_queue", { mode: GameMode.NORMAL });
   }
 
@@ -28,19 +30,34 @@ const Lobby = (props: any): JSX.Element => {
   return (
     <LobbyContainer>
       <div className="box">
+        <SmallButton
+          img_url={modalQuestionButton}
+          onClick={() => {
+            setGameManual(true);
+          }}
+        />
         <div className="body">
-          <BigButton
-            className="big"
-            img_url={normalButton}
-            onClick={normal_game_clicked}
-          />
-          <BigButton
-            className="big"
-            img_url={chaosButton}
-            onClick={special_game_clicked}
-          />
+          <BigButton img_url={normalButton} onClick={normal_game_clicked} />
+          <BigButton img_url={chaosButton} onClick={special_game_clicked} />
         </div>
       </div>
+
+      <Modal
+        onCloseModal={() => {
+          setGameManual(false);
+        }}
+        show={gameManual}
+        showCloseButton={true}
+      >
+        <div className="gameManual">
+          <p className="modeName normal">NormalMode</p>
+          <p className="manual">공과 패들이 충돌할 때 속도가 빨라집니다.</p>
+          <p className="manual normal">3점을 먼저 획득하면 승리합니다.</p>
+          <p className="modeName special">SpecialMode</p>
+          <p className="manual">NormalMode에서 다음이 추가됩니다.</p>
+          <p className="manual">위아래 벽 방향으로 중력을 받습니다.</p>
+        </div>
+      </Modal>
     </LobbyContainer>
   );
 };
@@ -74,6 +91,14 @@ const LobbyContainer = styled.div`
     box-sizing: border-box;
     /* width: fit-content; */
     padding: var(--html-padding-horizontal);
+    position: relative;
+    > Button {
+      position: absolute;
+      top: calc(var(--size) / 24);
+      left: calc(var(--size) / 24);
+      width: calc(var(--size) / 16);
+      height: calc(var(--size) / 16);
+    }
   }
   .top {
     width: 100%;
@@ -98,6 +123,38 @@ const LobbyContainer = styled.div`
       width: calc(var(--size) - 2 * var(--html-padding-horizontal));
       border: var(--border-width) solid black;
       border-radius: calc(50 / 800 * min(60vw, 800px));
+    }
+  }
+  .gameManual {
+    --modal-width: min(440px, calc(var(--body-width) * 0.3));
+    --modal-height: calc(var(--modal-width) * 6 / 5);
+    --width: var(--modal-width);
+    --height: var(--modal-height);
+    overflow: hidden;
+    .modeName {
+      line-height: calc(var(--height) * 0.1);
+      font-size: calc(var(--height) * 0.1);
+      font-weight: 500;
+      background-color: black;
+      border-radius: calc(var(--height) * 0.02);
+      margin: 0;
+      margin-top: calc(var(--height) * 0.1);
+      margin-bottom: calc(var(--height) * 0.05);
+      padding: 0;
+    }
+    .normal {
+      color: var(--color-yellow);
+    }
+    .special {
+      color: var(--color-red);
+    }
+    .manual {
+      padding: 0;
+      margin: 0;
+      line-height: calc(var(--width) / 100 * 7);
+      font-size: calc(var(--width) / 100 * 5);
+      text-align: left;
+      text-overflow: hidden;
     }
   }
 `;
