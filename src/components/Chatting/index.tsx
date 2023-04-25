d01dbf6270f279af86813f6b4d2adba37c94e787
ulol_wsl <jeffk0408@naver.com>
2023-04-25 15:16:08 +0900

    refactor : chatting menu 분리

--- src/components/Chatting/index.tsx
+++ src/components/Chatting/index.tsx
@@ -11,14 +11,7 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useQueryClient } from "react-query";
 import { UserInfo, useUserInfo } from "../../hooks/query/user";
 import { useGetChats, useChannelInfo } from "../../hooks/query/chat";
-import {
-  usePostChat,
-  useSetChannelPassword,
-  useAdmin,
-  useBan,
-  useKick,
-  useMute,
-} from "../../hooks/mutation/chat";
+import { usePostChat, useSetChannelPassword } from "../../hooks/mutation/chat";
 import { ChannelsInfo } from "../Channels/interface";
 import { Socket } from "socket.io-client";
 import { Scrollbars } from "react-custom-scrollbars";
@@ -38,121 +31,7 @@ import {
   Member,
   MemberType,
 } from "./interface";
-
-const ChatMenu = ({
-  userId,
-  channelInfo,
-  channelId,
-  socket,
-  setPopMenu,
-}: {
-  userId: string;
-  channelInfo: ChannelInfo;
-  channelId: string;
-  socket: Socket | undefined;
-  setPopMenu: React.Dispatch<React.SetStateAction<boolean>>;
-}) => {
-  const admin = useAdmin();
-  const kick = useKick();
-  const ban = useBan();
-  const mute = useMute();
-  const isMember = channelInfo.channelMembers
-    ?.map((member) => String(member.userId) === userId)
-    .includes(true);
-
-  const onAdminOther = () => {
-    admin.mutate({ id: channelId, user: userId, socket: socket });
-    setPopMenu(false);
-  };
-  const onKickOther = () => {
-    kick.mutate({ id: channelId, user: userId, socket: socket });
-    setPopMenu(false);
-  };
-  const onBanOther = () => {
-    ban.mutate({ id: channelId, user: userId, socket: socket });
-    setPopMenu(false);
-  };
-  const onMuteOther = () => {
-    mute.mutate({ id: channelId, user: userId, socket: socket });
-    setPopMenu(false);
-  };
-
-  useEffect(() => {
-    console.log(channelInfo.myType);
-  });
-
-  const OwnerRoll = (): JSX.Element => {
-    return (
-      <>
-        <div onClick={onAdminOther}>- Grant Admin</div>
-        <div onClick={onKickOther}>- Kick</div>
-        <div onClick={onBanOther}>- Ban</div>
-        <div onClick={onMuteOther}>- Mute</div>
-      </>
-    );
-  };
-
-  const AdminRoll = (): JSX.Element => {
-    return (
-      <>
-        {channelInfo.channelMembers?.map((member) => {
-          if (
-            String(member.userId) === userId &&
-            member.type === MemberType.OWNER
-          )
-            return <div>Channel Owner</div>;
-          else if (String(member.userId) === userId)
-            return (
-              <>
-                <div onClick={onKickOther}>Kick</div>
-                <div onClick={onBanOther}>Ban</div>
-                <div onClick={onMuteOther}>Mute</div>
-              </>
-            );
-          return null;
-        })}
-      </>
-    );
-  };
-
-  const MemberRoll = (): JSX.Element => {
-    return (
-      <>
-        {channelInfo.channelMembers?.map((member) => {
-          return String(member.userId) === userId
-            ? {
-                [MemberType.OWNER]: <div>Channel Owner</div>,
-                [MemberType.ADMIN]: <div>Channel Administrator</div>,
-                [MemberType.MEMBER]: <div>Channel Member</div>,
-              }[member.type]
-            : null;
-        })}
-      </>
-    );
-  };
-
-  return (
-    <ChatsMenuContainer>
-      <div
-        className="outMenu"
-        onClick={() => {
-          setPopMenu(false);
-        }}
-      ></div>
-      <div className="InMenu">
-        {isMember ? (
-          {
-            [MemberType.OWNER]: <OwnerRoll />,
-            [MemberType.ADMIN]: <AdminRoll />,
-            [MemberType.MEMBER]: <MemberRoll />,
-          }[channelInfo.myType]
-        ) : (
-          <div>Out Member</div>
-        )}
-      </div>
-    </ChatsMenuContainer>
-  );
-};
+import ChatMenu from "./menu";
 
 const ChatBubble = ({
   channelInfo,
