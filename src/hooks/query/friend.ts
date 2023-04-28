ee49e839fc58d42c9e890feae96f2db25b0833d6
keonwoo98 <keonwoo98@naver.com>
2023-04-28 15:05:23 +0900

    feat: blocklist api 추가

--- src/hooks/query/friend.ts
+++ src/hooks/query/friend.ts
@@ -21,7 +21,7 @@ export const usePendingFriendList = () => {
   const fetcher = useFetcher();
   const data = useQuery({
     queryKey: ['userPendingFriendList'],
-    queryFn: async() => {
+    queryFn: async () => {
       const response = await fetcher('/users/friends/pending', {
         method: 'GET',
         credentials: 'include',
@@ -37,8 +37,24 @@ export const useReceivedFriendList = () => {
   const fetcher = useFetcher();
   const data = useQuery({
     queryKey: ['userReceivedFriendList'],
-    queryFn: async() => {
+    queryFn: async () => {
       const response = await fetcher('/users/friends/received', {
+        method: 'GET',
+        credentials: 'include',
+      })
+      if (response.ok) return response.json();
+      throw response;
+    }
+  });
+  return data;
+}
+
+export const useBlocklist = () => {
+  const fetcher = useFetcher();
+  const data = useQuery({
+    queryKey: ['blocklist'],
+    queryFn: async () => {
+      const response = await fetcher('/users/friends/blocklist', {
         method: 'GET',
         credentials: 'include',
       })
