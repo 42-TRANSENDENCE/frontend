2f450cde6a52aa4c315f38ae9d39d829e36c876b
keonwoo98 <keonwoo98@naver.com>
2023-05-02 20:55:55 +0900

    Fix: 2FA에서 url로 home/chat/game 접근 금지

--- src/App/PrivateRoute.tsx
+++ src/App/PrivateRoute.tsx
@@ -2,26 +2,35 @@ import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
-const PrivateRoute = ({ component: Component }: { component: React.ComponentType<any> }) => {
+export const PrivateRoute = ({ component: Component }: { component: React.ComponentType<any> }) => {
   const awsUrl = `http://${import.meta.env.VITE_AWS_URL}:${import.meta.env.VITE_AWS_PORT}`;
   const [authenticated, setAuthenticated] = useState(true);
 
   useEffect(() => {
     const fetchUserData = async () => {
-      const response = await fetch(awsUrl + '/users', {
+      const userResponse = await fetch(awsUrl + '/users', {
         method: 'GET',
         credentials: 'include'
       })
-      if (response.status === 401) {
-        const response2 = await fetch(awsUrl + '/auth/refresh', {
+      if (userResponse.status === 404) {
+        setAuthenticated(false);
+      }
+      else if (userResponse.status === 401) {
+        const refreshResponse = await fetch(awsUrl + '/auth/refresh', {
           method: 'GET',
           credentials: 'include'
         })
-        if (response2.status === 401) {
+        if (refreshResponse.status === 401) {
           setAuthenticated(false);
         }
-      } else if (response.status === 404) {
-        setAuthenticated(false);
+        else {
+          const userResponse2 = await fetch(awsUrl + '/users', {
+            method: 'GET',
+            credentials: 'include'
+          })
+          if (userResponse2.status === 401)
+            setAuthenticated(false);
+        }
       }
     };
 
@@ -36,4 +45,28 @@ const PrivateRoute = ({ component: Component }: { component: React.ComponentType
   return <Component />;
 };
 
-export default PrivateRoute;
+export const TwoFactorRoute = ({ component: Component }: { component: React.ComponentType<any> }) => {
+  const awsUrl = `http://${import.meta.env.VITE_AWS_URL}:${import.meta.env.VITE_AWS_PORT}`;
+  const [authenticated, setAuthenticated] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const refreshResponse = await fetch(awsUrl + '/auth/refresh', {
+        method: 'GET',
+        credentials: 'include'
+      })
+      if (refreshResponse.status === 401) {
+        setAuthenticated(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (!authenticated) {
+    toast.warning('Login required');
+    return <Navigate to='/login' />;
+  }
+
+  return <Component />;
+};
